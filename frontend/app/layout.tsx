71fb779abb7346baf5d428648214498532784a2b
Alexey Dozortsev <alexwatcher@users.noreply.github.com>
2025-09-07 11:46:43 +0300

    test

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,9 +1,7 @@
-// export const dynamic = "force-dynamic";
 import "./globals.css";
 import MatrixRain from "../components/MatrixRain";
 import { LocaleProvider } from "../context/LocaleContext";
 import { NotificationProvider } from "../context/NotificationContext";
-import { ConfigProvider } from "@/context/ConfigContext";
 
 const availableLocales = ["en", "ru"];
 
@@ -12,10 +10,6 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const varName = "API_URL";
-  console.info("API_URL: ", process.env[varName]);
-
-  // const config = loadConfig();
   return (
     <html>
       <body className="h-screen w-screen">
